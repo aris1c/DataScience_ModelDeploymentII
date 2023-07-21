58af80185adb5f4708c7b69611499718f2f6ec76
Aris Candra <arisali537@gmail.com>
2023-07-22 00:06:30 +0700

    app.py

--- app.py
+++ app.py
@@ -1,24 +1,44 @@
+# main Python app
 import streamlit as st
 import streamlit.components.v1 as stc
 
-#IMPORT OUR APP
+#import our app
+from eda_app import run_eda_app
 from ml_app import run_ml_app
 
-def main():
-    st.title("Main App")
-    # stc.html(html_temp)
+html_temp = """
+            <div style="background-color:#3872fb;padding:10px;border-radius:10px">
+		    <h1 style="color:white;text-align:center;">Employee Promotion Prediction App </h1>
+		    <h4 style="color:white;text-align:center;">HR Team </h4>
+		    </div>
+            """
 
-    menu = ["Home","Machine Learning"]
+desc_temp = """
+            ### Employee Promotion Prediction App
+            This app will be used by the HR team to predict whether the employee get a promotion or not
+            #### Data Source
+            - https://raw.githubusercontent.com/aris1c/DataScience_ModelDeploymentII/main/human_capital.csv
+            #### App Content
+            - Exploratory Data Analysis
+            - Machine Learning Section
+            """
+
+
+def main():
+    # st.title("Main App")
+    stc.html(html_temp)
+
+    menu = ["Home","Machine Learning","Exploratory Data Analysis"]
     choice = st.sidebar.selectbox("Menu", menu)
 
     if choice == "Home":
         st.subheader("Home")
-        # st.markdown(desc_temp, unsafe_allow_html=True)
+        st.markdown(desc_temp, unsafe_allow_html=True)
     elif choice == "Machine Learning":
-        st.subheader("Machine Learning Section")
         run_ml_app()
+    elif choice == "Exploratory Data Analysis":
+        run_eda_app()
     
 
 if __name__ == '__main__':
     main()
-
